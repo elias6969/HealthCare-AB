aeea07d0d23c3fd03e40a0c9f8229daf75e013e1
elias6969 <eliasanderssons53@gmail.com>
2026-01-07 09:00:19 +0100

    fix sprint

--- HealthcareBooking.Api/obj/Debug/net8.0/HealthcareBooking.Api.AssemblyInfo.cs
+++ HealthcareBooking.Api/obj/Debug/net8.0/HealthcareBooking.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthcareBooking.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac34538b79eb3189d2b0cfbc84fd9552e10aac16")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthcareBooking.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthcareBooking.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
